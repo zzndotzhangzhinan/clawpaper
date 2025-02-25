be85415106443a73007d1a7a651958a0c77b1f66
zzndotzhangzhinan <67882626+zzndotzhangzhinan@users.noreply.github.com>
2025-02-25 15:35:19 -0500

    Rename figure-7.R to figure-E1.R

--- main/figure-7.R
+++
@@ -1,4 +0,0 @@
-source("coordinate_simu2.R")
-
-grid_arrange_shared_legend(line1.4fdr,line1.5fdr,line1.6fdr,
-                           line1.4tdp,line1.5tdp,line1.6tdp,nrow=2,ncol=3,position = "top")
---
+++ main/figure-E1.R
@@ -0,0 +1,4 @@
+source("coordinate_simu2.R")
+
+grid_arrange_shared_legend(line1.4fdr,line1.5fdr,line1.6fdr,
+                           line1.4tdp,line1.5tdp,line1.6tdp,nrow=2,ncol=3,position = "top")
