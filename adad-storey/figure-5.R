a1bcc2f715ac30cf53ebbf9c30805d4f8d7f55da
zzndotzhangzhinan <67882626+zzndotzhangzhinan@users.noreply.github.com>
2025-02-25 15:37:42 -0500

    Rename figure-5.R to figure-D2.R

--- adad-storey/figure-5.R
+++
@@ -1,5 +0,0 @@
-source("adad-storey.R")
-
-
-grid_arrange_shared_legend(line1.4fdr,line1.1fdr,line1.2fdr,
-                           line1.4tdp,line1.1tdp,line1.2tdp,nrow=2,ncol=3,position = "top")
